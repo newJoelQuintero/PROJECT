8fb2eaffb0336fbcc50f47cdc420161a8553b440
Camilo888888 <kamilosanchex045@gmail.com>
2025-06-20 08:11:05 -0500

    Docs: Se cambio la estructura de la bd

--- BASE_DE_DATOS/SQL/bd_NeuroEsencia.sql
+++ BASE_DE_DATOS/SQL/bd_NeuroEsencia.sql
@@ -9,26 +9,17 @@ dni_empleado VARCHAR(10) NOT NULL,
 nom_empleado VARCHAR(25)NOT NULL,
 dire_empleado VARCHAR(30)NOT NULL,
 fnaci_empleado DATE NOT NULL,
+usuario_empleado VARCHAR(40) NOT NULL,
+password_empleado VARCHAR(10) NOT NULL,
+email_empleado VARCHAR(40) NULL,
+tel_empleado VARCHAR(10) NOT NULL,
 rh_empleado VARCHAR(5)NOT NULL,
-UNIQUE(dni_empleado));
+UNIQUE(dni_empleado),
+CHECK(password_empleado >= 8));
 
-CREATE TABLE TEL_EMPLEADO(
-id_tel_empleado INT PRIMARY KEY AUTO_INCREMENT,
-tel_empleado BIGINT NOT NULL,
-id_empleado INT NOT NULL,
-FOREIGN KEY(id_empleado)REFERENCES EMPLEADO(id_empleado));
-
-CREATE TABLE EMAIL_EMPLEADO(
-id_email_empleado INT PRIMARY KEY AUTO_INCREMENT,
-email_empleado VARCHAR(40) NOT NULL,
-id_empleado INT NOT NULL,
-FOREIGN KEY(id_empleado)REFERENCES EMPLEADO(id_empleado));
-
-CREATE TABLE USUARIO_EMPLEADO(
+CREATE TABLE USUARIO(
 nom_usuario VARCHAR(40) PRIMARY KEY NOT NULL,
-password_usuario VARCHAR(25) NOT NULL,
-id_empleado INT NOT NULL,
-FOREIGN KEY(id_empleado)REFERENCES EMPLEADO(id_empleado));
+password_usuario VARCHAR(25) NOT NULL);
 
 CREATE TABLE CLIENTE(
 id_cliente INT PRIMARY KEY AUTO_INCREMENT,
@@ -37,23 +28,15 @@ dni_cliente VARCHAR(10) NOT NULL,
 nom_cliente VARCHAR(25)NOT NULL,
 direcc_cliente VARCHAR(30)NOT NULL,
 fnaci_cliente DATE NOT NULL,
+usuario_cliente VARCHAR(40) NOT NULL,
+password_cliente VARCHAR(10) NOT NULL,
+email_cliente VARCHAR(40) NULL,
 rh_cliente VARCHAR(5) NOT NULL,
+tel_cliente VARCHAR(10) NOT NULL,
 id_empleado INT NOT NULL,
 FOREIGN KEY(id_empleado)REFERENCES EMPLEADO(id_empleado),
-UNIQUE(dni_cliente));
-
-
-CREATE TABLE TEL_CLIENTE(
-id_tel_cliente INT PRIMARY KEY AUTO_INCREMENT,
-tel_cliente BIGINT NOT NULL,
-id_cliente INT NOT NULL,
-FOREIGN KEY(id_cliente)REFERENCES CLIENTE(id_cliente));
-
-CREATE TABLE EMAIL_CLIENTE(
-id_email_cliente INT PRIMARY KEY AUTO_INCREMENT,
-email_cliente VARCHAR(40) NOT NULL,
-id_cliente INT NOT NULL,
-FOREIGN KEY(id_cliente)REFERENCES CLIENTE(id_cliente));
+UNIQUE(dni_cliente, usuario_cliente),
+CHECK(password_cliente >= 8));
 
 CREATE TABLE CITA(
 id_cita BIGINT PRIMARY KEY NOT NULL,
@@ -72,7 +55,7 @@ FOREIGN KEY(id_empleado)references EMPLEADO(id_empleado));
 
 CREATE TABLE METODO_PAGO(
 id_metodo_pago INT PRIMARY KEY AUTO_INCREMENT,
-tipo_metodo VARCHAR(30));
+tipo_metodo VARCHAR(30) NOT NULL);
 
 CREATE TABLE FACTURA(
 id_factura BIGINT PRIMARY KEY NOT NULL,
