c686a86dff2015ba5deb129f4635dfc27925b24a
Karen1925 <karens_mosqueras@soy.sena.edu.co>
2025-06-25 23:07:51 -0500

    Update bd_NeuroEsencia.sql
    se modifico un campo

--- BASE_DE_DATOS/SQL/bd_NeuroEsencia.sql
+++ BASE_DE_DATOS/SQL/bd_NeuroEsencia.sql
@@ -42,7 +42,7 @@ CREATE TABLE CITA(
 id_cita BIGINT PRIMARY KEY NOT NULL,
 fecha_cita DATE NOT NULL,
 hora_cita TIME NOT NULL,
-valor_cita FLOAT,
+valor_cita FLOAT NOT NULL,
 id_empleado INT NOT NULL,
 FOREIGN KEY(id_empleado)references EMPLEADO(id_empleado));
 
